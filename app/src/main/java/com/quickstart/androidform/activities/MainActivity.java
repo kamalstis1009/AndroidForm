facd61d0023e04474b34cb91cd0e7ed125d6b786
Mustofa Kamal <mustofa2008@gmail.com>
2019-12-03 12:07:41 +0600

    Initial commit

--- app/src/main/java/com/quickstart/androidform/activities/MainActivity.java
+++ app/src/main/java/com/quickstart/androidform/activities/MainActivity.java
@@ -2,8 +2,10 @@ package com.quickstart.androidform.activities;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
@@ -14,11 +16,14 @@ import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
@@ -28,11 +33,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
+import com.quickstart.androidform.BuildConfig;
 import com.quickstart.androidform.R;
 import com.quickstart.androidform.models.User;
 import com.quickstart.androidform.utils.Utility;
 import com.quickstart.androidform.viewmodels.UserViewModel;
 
+import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -44,6 +52,8 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private static final int ACTION_PICK_REQUEST_CODE = 1;
     private static final int REQUEST_CODE = 2;
+    private static final int REQUEST_CODE_CAMERA = 3;
+    private static final int REQUEST_IMAGE_CAPTURE = 4;
     private ImageView imageView;
     private EditText name, birth;
     private Spinner districts;
@@ -53,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
     private String imageName;
     private String imagePath;
     private UserViewModel mUserViewModel;
+    private String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -86,11 +97,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             if (v.getId() == R.id.user_image) {
-                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE);
-                } else {
-                    getImage();
-                }
+                showDialog();
             }
             if (v.getId() == R.id.user_birth) {
                 Utility.getDate(MainActivity.this, birth);
@@ -114,7 +121,63 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void showDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View view = LayoutInflater.from(this).inflate(R.layout.dialog_photo_option, null, false);
+        builder.setView(view);
+        builder.setCancelable(true);
+        builder.create();
+        AlertDialog dialog = builder.show();
+        ((ImageButton) view.findViewById(R.id.camera_id)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, REQUEST_CODE_CAMERA);
+                } else {
+                    getCamera();
+                }
+                dialog.dismiss();
+            }
+        });
+        ((ImageButton) view.findViewById(R.id.gallery_id)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE);
+                } else {
+                    getImage();
+                }
+                dialog.dismiss();
+            }
+        });
+    }
+
     //====================================================| For Image
+    private void getCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            try {
+                File photoFile = createImageFile();
+                if (photoFile != null) {
+                    Uri photoURI = FileProvider.getUriForFile(MainActivity.this, BuildConfig.APPLICATION_ID + ".provider", photoFile);
+                    Log.d(TAG, "Image Uri: "+photoURI);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                    startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName,/* prefix */".jpg",/* suffix */storageDir/* directory */);
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
     private void getImage() {
         Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(intent, ACTION_PICK_REQUEST_CODE);
@@ -132,6 +195,10 @@ public class MainActivity extends AppCompatActivity {
                 imagePath = Utility.saveToInternalStorage(MainActivity.this, bm, imageName);
             }
         }
+        if (requestCode == REQUEST_IMAGE_CAPTURE && currentPhotoPath != null) {
+            Uri uri = Uri.fromFile(new File(currentPhotoPath));
+            imageView.setImageURI(uri);
+        }
     }
 
     @Override
@@ -139,6 +206,9 @@ public class MainActivity extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode==REQUEST_CODE && grantResults[0]==PackageManager.PERMISSION_GRANTED) {
             getImage();
+        }
+        if (requestCode == REQUEST_CODE_CAMERA) {
+            getCamera();
         }
     }
 
